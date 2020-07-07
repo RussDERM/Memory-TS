e1dc7f3d5af6e7113f2b93c57161f3a0d1692ee3
Russell Dermody <russ.dermody@gmail.com>
2020-07-07 14:26:46 -0700

    Tile updates to TSX

--- src/components/tile/tile.tsx
+++ src/components/tile/tile.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./tile.css";
-
-// * Random Color
 const randomColor = import("randomcolor");
+
+// interface TileColor {
+
+// }
+
+// // * Random Color
+// let tileColor = randomColor: Promise<{
+//     default: {
+//         (luminosity?: "bright" | undefined): string;
+//     };
+// }>({luminosity: "bright"})
 
 // class TileComponent extends React.Component {
 //   state = {
@@ -13,21 +22,24 @@ const randomColor = import("randomcolor");
 //     this.setState({ count: this.state.count + 1 });
 //   };
 
-const TileComponent = (props: ) => {
+const TileComponent = (props: {
+  id: number;
+  icon: string;
+  count: number;
+  handleClick: any;
+}) => {
   return (
     // *on click, deliver tile id prop to handleClick function
     <div className="tile" onClick={() => props.handleClick(props.id)}>
       <i
         style={{
-          color: randomColor({
-            luminosity: "bright"
-          })
+          color: "lightBlue"
         }}
         className={props.icon}
       />
     </div>
   );
-}
+};
 // }
 
 export default TileComponent;
